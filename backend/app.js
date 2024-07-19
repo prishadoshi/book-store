7b4e96b414c295deafd474105960fea3b1e5e008
prishadoshi <prisha.doshi123@gmail.com>
2024-07-19 14:49:51 +0300

    create favourite books api

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,12 @@ require("dotenv").config();
 require("./conn/conn.js");
 const User = require("./routes/user.js");
 const Books = require("./routes/book.js");
+const Favourite = require("./routes/favourite.js");
+
 //routes
 app.use("/api/v1", User);
 app.use("/api/v1", Books);
+app.use("/api/v1", Favourite);
 
 //creating Port
 app.listen(process.env.PORT, () => {
