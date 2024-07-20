90db0f0692822e2cf1850d8fd8016afe94ca2c75
prishadoshi <prisha.doshi123@gmail.com>
2024-07-20 21:24:55 +0300

    create bookdetails page

--- frontend/src/pages/AllBooks.jsx
+++ frontend/src/pages/AllBooks.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import BookCard from "../components/BookCard/BookCard";
+import Loader from "../components/Loader/Loader";
 
 const AllBooks = () => {
-  return <div>Allbooks</div>;
+  const [Data, setData] = useState();
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get(
+        "http://localhost:1000/api/v1/get-all-books"
+      );
+      setData(response.data.data);
+    };
+    fetch();
+  }, []);
+
+  return (
+    <div className="bg-zinc-900 h-auto px-12 py-8">
+      {" "}
+      <h1 className="text-3xl text-yellow-100">All Books</h1>
+      {!Data && (
+        <div className="flex flex-items justify-center my-8">
+          <Loader />{" "}
+        </div>
+      )}
+      <div className="my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
+        {Data &&
+          Data.map((items, i) => (
+            <div key={i}>
+              <BookCard data={items} />{" "}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllBooks;
