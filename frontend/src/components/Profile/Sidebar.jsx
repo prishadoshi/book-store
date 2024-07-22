67d984603b2ec6f28121b4d05a16e0bba18950c6
prishadoshi <prisha.doshi123@gmail.com>
2024-07-22 10:54:51 +0300

    implement settings and mobile Navbar

--- frontend/src/components/Profile/Sidebar.jsx
+++ frontend/src/components/Profile/Sidebar.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 import { FaArrowRightFromBracket } from "react-icons/fa6";
 const Sidebar = ({ data }) => {
   return (
-    <div className="bg-zinc-800 p-4 rounded flex flex-col items-center justify-between h-[100%]">
+    <div className="bg-zinc-800 p-4 rounded flex flex-col items-center justify-between h-auto lg:h-[100%]">
       <div className="flex items-center flex-col justify-center">
+        {" "}
         <img src={data.avatar} className="h-[12vh]" alt="" />
         <p className="mt-3 text-xl text-zinc-100 font-semibold">
           {data.username}
@@ -33,7 +34,7 @@ const Sidebar = ({ data }) => {
           Settings
         </Link>
       </div>
-      <button className="bg-zinc-900 w-3/6 lg:w-full mt4 lg:mt-0 text-white font-semibold flex items-center justify-center py-2 rounded hover:bg-white hover:text-zinc-900 transition-all duration-300">
+      <button className="bg-zinc-900 w-3/6 lg:w-full mt-4 lg:mt-0 text-white font-semibold flex items-center justify-center py-2 rounded hover:bg-white hover:text-zinc-900 transition-all duration-300">
         Log Out <FaArrowRightFromBracket className="ms-4" />
       </button>
     </div>
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Loader from "../components/Loader/Loader";
 import axios from "axios";
+import MobileNav from "../components/Profile/MobileNav";
 
 const Profile = () => {
   //const isLoggedIn = useSelector();
@@ -24,7 +25,7 @@ const Profile = () => {
   }, []);
 
   return (
-    <div className="bg-zinc-900 px-2 py-8 md:px-12 flex flex-col md:flex-row h-screen gap-4 text-white">
+    <div className="bg-zinc-900 px-2 py-8 md:px-12 flex flex-col md:flex-row gap-4 text-white">
       {!Profile && (
         <div className="w-full h-[100%] flex items-center justify-center">
           <Loader />
@@ -32,10 +33,11 @@ const Profile = () => {
       )}
       {Profile && (
         <>
-          <div className=" w-full md:w-1/6">
+          <div className="w-full md:w-1/6 h-auto lg:h-screen">
             <Sidebar data={Profile} />
+            <MobileNav />
           </div>
-          <div className="w-5/6">
+          <div className="w-full md:w-5/6">
             <Outlet />
           </div>
         </>
